69bfcbb6f2d6721798d5593c7bd3d01c2ef693d3
Ruchi Bahety <ruchi.malpani@gmail.com>
2018-04-16 17:45:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  var new_object = Object.assign({}, {key, value})
+  return new_object
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key,value) {
